d231a11aea3de329e94545b7be344563654e5ee0
Jihad tarek <jihadtarek60@gmail.com>
2020-06-15 22:52:29 +0200

    Optimize&MiniCSS

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,17 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const TerserPlugin = require('terser-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin =require('optimize-css-assets-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({}),new OptimizeCSSAssetsPlugin({})],
+      },
     module: {
         rules: [
             {
@@ -16,24 +21,16 @@ module.exports = {
                 loader: "babel-loader"
             },
             {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader','sass-loader'],
-            },
-            {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             } 
         ]
     },
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin()],
-      },
     plugins: [
-        new MiniCssExtractPlugin({filename:["[name].css"]}),
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({filename:'[name].css'}),
     ]
 }
