6ed8a3dce1a46cde4cd101d5d2325d4b00f0ed60
Jihad tarek <jihadtarek60@gmail.com>
2020-06-15 22:51:53 +0200

    output

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,6 +9,10 @@ module.exports = {
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+      },
     module: {
         rules: [
             {
@@ -27,7 +31,9 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new BundleAnalyzerPlugin(),
+        new BundleAnalyzerPlugin({
+            analyzerMode:'server',
+        }),
         new CleanWebpackPlugin({
             // Simulate the removal of files
             dry: true,
