5109e936d608e704b7c472b60521a78e031888b6
Jihad tarek <jihadtarek60@gmail.com>
2020-06-15 03:20:39 +0200

    formHandler file

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,14 +2,29 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let formText = document.getElementById('textForm').value
+    //checkForName(formText)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    //console.log("::: Form Submitted :::")
+    if(!formText) return;
+    console.log(formText)
+    fetch('/article',{
+        method:'POST',
+        mode:'cors',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify({text : formText})
+    })
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        console.log(res)
+
+        document.getElementById('polarity').innerHTML = res.polarity
+        document.getElementById('polarity_confidence').innerHTML = res.polarity_confidence
+        document.getElementById('subjectivity').innerHTML = res.subjectivity
+        document.getElementById('subjectivity_confidence').innerHTML = res.subjectivity_confidence
+        document.getElementById('excerpt').innerHTML = res.text
     })
 }
 
