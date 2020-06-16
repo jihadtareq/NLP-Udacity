08c087b99ea5ab2eeb46f8f2ee743c63fc088039
Jihad tarek <jihadtarek60@gmail.com>
2020-06-16 05:15:02 +0200

    install body-parser & put alyien

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,17 @@
 const dotenv = require('dotenv');
 dotenv.config();
 var path = require('path')
-var aylien = require("aylien_textapi");
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var bodyParser = require('body-parser')
+const mockAPIResponse = require('./mockAPI.js');
 
 const app = express()
+ 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(express.static('dist'))
 
@@ -13,6 +19,7 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
      res.sendFile('dist/index.html')
+     return
     //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
@@ -23,10 +30,54 @@ app.listen(8080, function () {
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+    return
 })
 
 // set aylien API credentias
-var textapi = new aylien({
-    application_id: process.env.API_ID,
-    application_key: process.env.API_KEY
-    });
+
+function postAlyien(req,res,next)
+{
+  var AYLIENTextAPI = require('aylien_textapi');
+  var textapi = new AYLIENTextAPI({
+    application_id:process.env.API_ID,
+    application_key:process.env.API_KEY
+  });
+  textapi.entities({
+     'text': req.body
+    //text:req.body.text
+  }, function(error, response) {
+    if (error === null) {
+      Object.keys(response.entities).forEach(function(e) {
+        console.log(e + ": " + response.entities[e].join(","));
+      });
+    }
+    console.log(response)
+    res.send(response)
+  });
+}
+
+
+function postAlyienURL(req,res,next)
+{
+  var AYLIENTextAPI = require('aylien_textapi');
+  var textapi = new AYLIENTextAPI({
+    application_id:process.env.API_ID,
+    application_key:process.env.API_KEY
+  });
+  textapi.sentiment({
+    //url:'https://www.newscientist.com/article-topic/love/',
+    'url':req.body.text,
+    mode: 'tweet'
+  }, function(error, response) {
+    if (error === null) {
+      console.log(response);
+      res.send(response)
+    }
+  });
+}
+
+
+//Post
+
+app.post('/api',postAlyien)
+app.post('/article',postAlyienURL)
