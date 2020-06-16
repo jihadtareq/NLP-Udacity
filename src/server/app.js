8554c66b32efb481460ee7591e0cdadbc38d3f28
Jihad tarek <jihadtarek60@gmail.com>
2020-06-17 00:55:05 +0200

    cors

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,10 @@
 var path = require('path')
 const express = require('express')
-var bodyParser = require('body-parser')
 var cors = require('cors')
+const app = express()
+var bodyParser = require('body-parser')
 const mockAPIResponse = require('./mockAPI.js');
 var requestPost = require('./index')
-const app = express()
 
 app.use(cors())
 // parse application/x-www-form-urlencoded
